230ff99cd981b7384fffee8839998aaa52db2c26
hmc <hmc2020925@gmail.com>
2025-04-10 22:49:07 -0600

    Prepared assets and database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+
+const sound = ['public/assets/g2.wav', 'public/assets/g3.wav', 'public/assets/a2.wav', 'public/assets/a3.wav', 'public/assets/c3.wav', 'public/assets/c4.wav', 'public/assets/d3.wav', 'public/assets/d4.wav', 'public/assets/e3.wav', 'public/assets/e4.wav'];
+const song = ['public/assets/song1.mp3', 'public/assets/song2.mp3'];
+const inactivemon = ['public/assets/g_inactive.png', 'public/assets/a_inactive.png', 'public/assets/c_inactive.png', 'public/assets/d_inactive.png', 'public/assets/e_inactive.png'];
+const activemon = ['public/assets/g_active.png', 'public/assets/a_active.png', 'public/assets/c_active.png', 'public/assets/d_active.png', 'public/assets/e_active.png'];
+
+
 function App() {
+
+  const [background, setBackground] = useState('public/assets/firstpage.png');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
 
